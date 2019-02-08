aff1c47e8320cb2e6481deeca3da4e7c79e4a049
lucia124 <47173746+lucia124@users.noreply.github.com>
2019-02-08 10:11:33 +0100

    Update config.php
    hemos cambiado el localhost  y el nombre de pedidos

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php //GLOBAL $db;
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', '10.129.7.156');
    define('DB_USERNAME', 'root');
    //define('DB_PASSWORD', '');
    define('DB_PASSWORD', 'rootroot');
-   define('DB_DATABASE', 'pedidos');
+   define('DB_DATABASE', 'classicmodels');
      $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if (!$db) {
